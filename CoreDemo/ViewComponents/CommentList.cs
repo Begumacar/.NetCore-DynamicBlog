3fc90782a19f70b015c9fee171374c740b7ee578
Begüm <begumacaar@gmail.com>
2021-10-09 17:37:44 +0300

    updated
    9.10.21

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Begüm"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Beril"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="İlk"
+                },
+
+            };
+            return View(commentvalues);
+        }
+    }
+}
